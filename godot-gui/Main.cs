7cd9160d332769616cc35fed8e303a112ec0345e
ByzantineProcess <87960803+ByzantineProcess@users.noreply.github.com>
2026-03-02 22:19:39 +1100

    routing system

--- godot-gui/Main.cs
+++ godot-gui/Main.cs
@@ -3,13 +3,12 @@ using System;
 
 public partial class Main : Control
 {
-	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
     {
         GetTree().Root.SizeChanged += OnSizeChanged;
+        OnSizeChanged();
     }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
     {
         
@@ -17,10 +16,15 @@ public partial class Main : Control
 
     public void OnSizeChanged()
     {
-        GD.Print("Window size changed: " + GetWindow().Size);
         if (GetWindow().Size.X < 900 || GetWindow().Size.Y < 500)
         {
-            GetTree().ChangeSceneToFile("res://godot-gui/too_small.tscn");
+            GetNode<Control>("TooSmall").Visible = true;
+            GetNode<Control>("Main").Visible = false;
+        }
+        else
+        {
+            GetNode<Control>("TooSmall").Visible = false;
+            GetNode<Control>("Main").Visible = true;
         }
     }
 }
---
+++ godot-gui/router/Router.cs
@@ -0,0 +1,24 @@
+using Godot;
+using System;
+using System.Linq;
+
+public partial class Router : Node
+{
+    PackedScene gui = GD.Load<PackedScene>("res://godot-gui/main.tscn");
+	public override void _Process(double delta)
+    {
+        var args = OS.GetCmdlineArgs();
+        if (args.Contains("--service"))
+        {
+           // TODO: service mode, --headless is assumed.
+        }
+        else
+        {
+            GD.Print("Starting GUI");
+            // otherwise, load the gui
+            GetTree().ChangeSceneToPacked(gui);
+        }
+
+        
+    }
+}
